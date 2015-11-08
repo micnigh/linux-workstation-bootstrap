df6388b9744f57ae0086f878aacde040051a0cf5
Michael Nigh <mic.nigh@gmail.com>
2015-11-07 19:44:23 -0800

    Add user profile script

---
+++ scripts/setup-user-profile.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# apm packages
+apm install \
+  minimap \
+    minimap-git-diff \
+    minimap-find-and-replace \
+    minimap-selection \
+    minimap-pigments \
+  linter \
+    linter-eslint \
+    linter-htmlhint \
+    linter-less \
+    linter-scss-lint \
+  pigments \
+  line-ending-converter \
+  git-plus \
+  merge-conflicts \
+  language-docker \
+  language-babel \
+  file-type-icons \
+  file-icons \
+  tabs-to-spaces \
+  Sublime-Style-Column-Selection \
+  color-picker \
+  editorconfig \
+  imdone-atom
