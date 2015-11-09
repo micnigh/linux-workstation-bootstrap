379801fd0e532eb303035b3a9ece14d04ea1e9c4
Michael Nigh <mic.nigh@gmail.com>
2015-11-08 19:25:01 -0800

    Setup saner defaults

--- scripts/distro/ubuntu-based/ubuntu-unity/setup-user-profile.sh
+++ scripts/distro/ubuntu-based/ubuntu-unity/setup-user-profile.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+# disable online search lenses - http://askubuntu.com/a/629255
+gsettings set com.canonical.Unity.Lenses remote-content-search 'none'
+
+# add calc to default search - http://askubuntu.com/a/594161, http://discourse.ubuntu.com/t/improve-unity-offline-scopes-performance/1202/6
+gsettings set com.canonical.Unity.Lenses always-search "['applications.scope', 'info-calculator.scope', 'music.scope', 'videos.scope', 'files.scope']"
+
+# enable virtual desktops
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 2
+gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 2
