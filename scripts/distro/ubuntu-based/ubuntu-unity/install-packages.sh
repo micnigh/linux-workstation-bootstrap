f41ead13d00d0f619cb747c91a07c93663d97af1
Michael Nigh <mic.nigh@gmail.com>
2015-11-08 19:24:51 -0800

    Disable online search

--- scripts/distro/ubuntu-based/ubuntu-unity/install-packages.sh
+++ scripts/distro/ubuntu-based/ubuntu-unity/install-packages.sh
@@ -4,3 +4,15 @@
 sudo systemctl enable docker
 
 sudo apt-get install -y unity-tweak-tool
+
+# disable search globally - http://askubuntu.com/a/629255
+sudo tee /etc/xdg/autostart/disable_onlinesearch.desktop <<EOF
+[Desktop Entry]
+Name=Disable Search
+Exec=/bin/bash -c "gsettings set com.canonical.Unity.Lenses remote-content-search 'none'"
+Type=Application
+EOF
+
+# remove amazon launcher - http://askubuntu.com/a/629255
+sudo apt-get purge -y unity-webapps-common
+sudo rm /usr/share/applications/ubuntu-amazon-default.desktop
