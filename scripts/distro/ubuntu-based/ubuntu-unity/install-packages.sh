4989d4ca600bf950c077c603f2e6bf26b8075141
Michael Nigh <mdnigh@nps.edu>
2015-11-12 11:01:34 -0800

    Add workaround to keep unity-tweak-tool without installing unity-webapps-service

--- scripts/distro/ubuntu-based/ubuntu-unity/install-packages.sh
+++ scripts/distro/ubuntu-based/ubuntu-unity/install-packages.sh
@@ -5,6 +5,17 @@ sudo systemctl enable docker
 
 sudo apt-get install -y unity-tweak-tool
 
+# workaround so we dont need to install unity-webapps-service package
+# http://askubuntu.com/a/653634
+TEMP_DIR=$(mktemp)
+cd $TEMP_DIR
+apt-get download unity-webapps-service
+ar xvf *
+tar xvf data*
+mv usr/share/glib-2.0/schemas/com.canonical.unity.webapps.gschema.xml /usr/share/glib-2.0/schemas/
+glib-compile-schemas /usr/share/glib-2.0/schemas/
+rm -rf $TEMP_DIR
+
 # disable search globally - http://askubuntu.com/a/629255
 sudo tee /etc/xdg/autostart/disable_onlinesearch.desktop <<EOF
 [Desktop Entry]
